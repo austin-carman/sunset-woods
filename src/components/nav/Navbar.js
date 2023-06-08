28cb62cc4d54f656a23bab7f6b2bb724e6b72066
austin-carman <abcarman12@gmail.com>
2023-06-08 12:16:06 -1000

    render nav links with dropdown on shop link

---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdown = () => {
+    console.log("hello");
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <nav>
+      <a href="/">
+        <img src="#" alt="Company Logo" />
+        Company Name
+      </a>
+      <div>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="" onMouseOver={handleDropdown} onMouseOut={handleDropdown}>
+              Shop
+            </a>
+            {isDropdownOpen && (
+              <div>
+                <a href="">About Us</a>
+                <a href="">FAQ</a>
+              </div>
+            )}
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+          <li>
+            <a href="">Cart</a>
+          </li>
+          <li>
+            <a href="">Custom Order</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
