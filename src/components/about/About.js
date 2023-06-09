ab6aabefc0d3727ebe0eb8231afdd221818cfb08
austin-carman <abcarman12@gmail.com>
2023-06-09 12:50:36 -1000

    added page content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,19 +1,29 @@
 const About = () => {
+  // eslint-disable-next-line no-undef
+  const image = process.env.PUBLIC_URL + "./blue.png";
   return (
     <>
-      <div>
-        <h2></h2>
-        <h4></h4>
-      </div>
-      <div>
-        <div>
-          <h2></h2>
-          <p></p>
-        </div>
-        <div>
-          <img src="#" alt="" />
+      <div className="about-hero-section">
+        <div className="about-hero-content">
+          <h2>About Us</h2>
+          <h4>Quis in ea ex occaecat labore.</h4>
         </div>
       </div>
+      <section className="about-section">
+        <div className="about-content">
+          <h2>About Us</h2>
+          <p>
+            Cupidatat ipsum aliquip officia ea adipisicing nostrud pariatur
+            voluptate cupidatat Lorem ad elit et elit. Mollit ipsum esse nisi
+            veniam cupidatat Lorem esse non officia. Consectetur proident enim
+            fugiat dolore laboris quis excepteur Lorem aute dolor mollit labore
+            veniam dolore. Deserunt duis eu cupidatat officia tempor sint.
+          </p>
+        </div>
+        <div className="about-img-container">
+          <img src={image} alt="" />
+        </div>
+      </section>
     </>
   );
 };
