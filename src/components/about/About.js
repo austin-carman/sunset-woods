d3635bf01f1b1752142fd0359d99d336fc428fb6
austin-carman <abcarman12@gmail.com>
2023-06-09 12:43:33 -1000

    basic jsx structure

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,21 @@
 const About = () => {
-  return "About";
+  return (
+    <>
+      <div>
+        <h2></h2>
+        <h4></h4>
+      </div>
+      <div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <img src="#" alt="" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
