bb62dda041d748d9d7658ac2bca13fc8e1ca9795
austin-carman <abcarman12@gmail.com>
2023-06-08 20:31:55 -1000

    rendering basic structure for home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,42 @@
 const Home = () => {
-  return <div style={{ height: "100vh" }}>Home</div>;
+  return (
+    <>
+      {/* Hero image and Call to Action */}
+      <div>
+        <img src="#" alt="" />
+        <button>Call to Action</button>
+      </div>
+      {/* Gallery */}
+      <section>
+        <div>
+          <img src="" alt="" />
+          <h4>Category title</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>Category title</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>Category title</h4>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Title</h2>
+          <p>
+            Nisi non consectetur occaecat est aliqua. Quis id incididunt in
+            nostrud quis. Cillum aute nisi ea laboris Lorem culpa minim proident
+            ad veniam exercitation minim mollit veniam. Anim dolore sit nostrud
+            et pariatur ullamco laborum sint aute eiusmod aute tempor cillum
+            adipisicing. Consectetur eiusmod labore dolor fugiat cillum mollit
+            aliqua duis. Ad do ipsum aute sit deserunt.
+          </p>
+        </div>
+        <img src="" alt="" />
+      </section>
+    </>
+  );
 };
 
 export default Home;
