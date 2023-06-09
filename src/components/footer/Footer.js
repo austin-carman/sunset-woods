3ec924b0ef038df047948ce1a9e0414826d4f552
austin-carman <abcarman12@gmail.com>
2023-06-09 11:57:12 -1000

    footer styles media queries

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,53 +3,65 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <div className="info-container">
-        <div className="img-links-container">
-          <div className="img-quick-links-container">
-            <img src="#" alt="company logo" />
-            <div className="links-container">
-              <ul className="quick-links">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/shop">Shop</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                  <Link to="/cart">Cart</Link>
-                </li>
-                <li>
-                  <Link to="/custom-orders">Custom Orders</Link>
-                </li>
-              </ul>
-              <ul className="quick-links">
-                <li>Contact</li>
-                <li>Call or Text:</li>
-                <li>
-                  <a href="tel:+44-785-7895">+44-785-7895</a>
-                </li>
-                <li>
-                  <a href="mailto:1234@example.com">Email Us</a>
-                </li>
-                <li>
-                  <Link to="/custom-orders">
-                    <button>Custom Quote</button>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="social-links-container">
-            <Link to="https://www.instagram.com/sunsettrailwoods/">
-              Instagram
-            </Link>
-          </div>
+      <div className="links-container">
+        <div className="footer-section-container">
+          <img src="#" alt="company logo" />
+        </div>
+        <div className="footer-section-container">
+          <ul className="quick-links">
+            <li>
+              <span className="links-label">Links</span>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+            <li>
+              <Link to="/custom-orders">Custom Orders</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section-container">
+          <ul className="quick-links">
+            <li>
+              <span className="links-label">Contact</span>
+            </li>
+            <li>Call or Text:</li>
+            <li>
+              <a href="tel:+44-785-7895">+44-785-7895</a>
+            </li>
+            <li>
+              <a href="mailto:1234@example.com">Email Us</a>
+            </li>
+            <li>
+              <Link to="/custom-orders">
+                <button>Custom Quote</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section-container">
+          <ul className="social-links-container">
+            <li>
+              <span className="links-label">Follow Us</span>
+            </li>
+            <li>
+              <Link to="https://www.instagram.com/sunsettrailwoods/">
+                Instagram
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="copyright">
