918123c5c566091ad852e4a91f2afa47e413608f
austin-carman <abcarman12@gmail.com>
2023-06-12 12:00:50 -1000

    basic structure and styles for shop category menu buttons

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Shop = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [selected, setSelected] = useState("");
   // eslint-disable-next-line no-undef
-  // const image = process.env.PUBLIC_URL + "./blue.png";
+  const image = process.env.PUBLIC_URL + "./blue.png";
+
+  const handleClickCategory = (category) => {
+    setSelected(category);
+  };
 
   return (
     <>
@@ -17,7 +24,74 @@ const Shop = () => {
           <button className="shop-custom-order-button">Custom Order</button>
         </Link>
       </div>
-      <section></section>
+      <section className="shop-categories">
+        <div
+          className={
+            selected === "all"
+              ? "shop-categories-item selected"
+              : "shop-categories-item"
+          }
+          onClick={() => handleClickCategory("all")}
+        >
+          <img src={image} alt="" />
+          <h4>All Products</h4>
+        </div>
+        <div
+          className={
+            selected === "furniture"
+              ? "shop-categories-item selected"
+              : "shop-categories-item"
+          }
+          onClick={() => handleClickCategory("furniture")}
+        >
+          <img src={image} alt="" />
+          <h4>Furniture</h4>
+        </div>
+        <div
+          className={
+            selected === "signs"
+              ? "shop-categories-item selected"
+              : "shop-categories-item"
+          }
+          onClick={() => handleClickCategory("signs")}
+        >
+          <img src={image} alt="" />
+          <h4>Signs</h4>
+        </div>
+        <div
+          className={
+            selected === "boards"
+              ? "shop-categories-item selected"
+              : "shop-categories-item"
+          }
+          onClick={() => handleClickCategory("boards")}
+        >
+          <img src={image} alt="" />
+          <h4>Cutting Boards</h4>
+        </div>
+        <div
+          className={
+            selected === "coasters"
+              ? "shop-categories-item selected"
+              : "shop-categories-item"
+          }
+          onClick={() => handleClickCategory("coasters")}
+        >
+          <img src={image} alt="" />
+          <h4>Coasters</h4>
+        </div>
+        <div
+          className={
+            selected === "other"
+              ? "shop-categories-item selected"
+              : "shop-categories-item"
+          }
+          onClick={() => handleClickCategory("other")}
+        >
+          <img src={image} alt="" />
+          <h4>Other</h4>
+        </div>
+      </section>
     </>
   );
 };
