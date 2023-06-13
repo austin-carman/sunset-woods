c712b5d62b1dc08116bd1a4f65e9340766c2b8b9
austin-carman <abcarman12@gmail.com>
2023-06-13 12:30:59 -1000

    cart basic structure and styles

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,5 +1,16 @@
 const Cart = () => {
-  return "Cart";
+  return (
+    <div className="cart">
+      <div>
+        <h3>Cart</h3>
+        <h4>x</h4>
+      </div>
+      <div>
+        <h5>Item</h5>
+        <img src="" alt="cart item" />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
