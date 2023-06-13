4bbb633eb3d2956d23faa615ebc75def6d21a144
austin-carman <abcarman12@gmail.com>
2023-06-13 10:15:28 -1000

    contact page general styles

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 const Contact = () => {
   return (
     <>
+      {/* <div className="contact-hero-container"> */}
       <div className="contact-hero-container">
         <div className="contact-hero-content">
           <h2>Contact</h2>
@@ -8,32 +9,34 @@ const Contact = () => {
             Minim aliqua est non ipsum est veniam ullamco non tempor anim.
           </h4>
         </div>
-        <div className="contact-container">
-          <div className="info-container">
-            <h3>Contact Info:</h3>
-            <h6>
-              Call: <a href="tel:+44-785-7895">+44-785-7895</a>
-            </h6>
-            <h6>
-              Email:{" "}
-              <a href="mailto:abcarman12@gmail.com">abcarman12@gmail.com</a>
-            </h6>
-            <h6>
-              Instagram:{" "}
-              <a href="https://www.instagram.com/sunsettrailwoods/">
-                @sunsettrailwoods
-              </a>
-            </h6>
-          </div>
-          <div className="contact-form">
-            <h3>Send us a message</h3>
-            <input placeholder="Name"></input>
-            <input placeholder="Phone"></input>
-            <input placeholder="Email"></input>
-            <input placeholder="Message"></input>
-          </div>
+      </div>
+      <div className="contact-container">
+        <div className="info-container">
+          <h3>Contact Info</h3>
+          <h6>
+            Call: <a href="tel:+44-785-7895">+44-785-7895</a>
+          </h6>
+          <h6>
+            Email:{" "}
+            <a href="mailto:abcarman12@gmail.com">abcarman12@gmail.com</a>
+          </h6>
+          <h6>
+            Instagram:{" "}
+            <a href="https://www.instagram.com/sunsettrailwoods/">
+              @sunsettrailwoods
+            </a>
+          </h6>
+        </div>
+        <div className="contact-form">
+          <h3>Send us a message</h3>
+          <input placeholder="Name"></input>
+          <input placeholder="Phone"></input>
+          <input placeholder="Email"></input>
+          <textarea placeholder="Message" rows="10"></textarea>
+          <button>Send Message</button>
         </div>
       </div>
+      {/* </div > */}
     </>
   );
 };
