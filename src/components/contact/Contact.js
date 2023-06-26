a1237f6e6ae282d9a63abe8d6f74ca90a5afd84c
austin-carman <abcarman12@gmail.com>
2023-06-26 12:49:32 -1000

    created slice of state for contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,22 @@
 import HeroContent from "../hero/HeroContent";
+import { useState } from "react";
 
 const Contact = () => {
+  const initialState = {
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+  };
+  const [messageForm, setMessageForm] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMessageForm({ ...messageForm, [name]: value });
+  };
+
+  console.log("message: ", messageForm);
+
   return (
     <>
       {/* Hero image and Call to Action */}
@@ -14,11 +30,10 @@ const Contact = () => {
         <div className="info-container">
           <h3>Contact Info</h3>
           <h6>
-            Call: <a href="tel:+44-785-7895">+44-785-7895</a>
+            Call: <a href="tel:+44-785-7895">012-345-6789</a>
           </h6>
           <h6>
-            Email:{" "}
-            <a href="mailto:abcarman12@gmail.com">abcarman12@gmail.com</a>
+            Email: <a href="mailto:abcarman12@gmail.com">email@email.com</a>
           </h6>
           <h6>
             Instagram:{" "}
@@ -26,14 +41,36 @@ const Contact = () => {
               @sunsettrailwoods
             </a>
           </h6>
-          {/* <img src={image} alt="" /> */}
         </div>
         <div className="contact-form">
           <h3>Send us a message</h3>
-          <input placeholder="Name"></input>
-          <input placeholder="Phone"></input>
-          <input placeholder="Email"></input>
-          <textarea placeholder="Message" rows="10"></textarea>
+          <input
+            placeholder="Name"
+            onChange={handleChange}
+            name="name"
+            value={messageForm.name}
+          ></input>
+          <input
+            type="tel"
+            placeholder="Phone"
+            onChange={handleChange}
+            name="phone"
+            value={messageForm.phone}
+          ></input>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            name="email"
+            value={messageForm.email}
+          ></input>
+          <textarea
+            placeholder="Message"
+            rows="10"
+            onChange={handleChange}
+            name="message"
+            value={messageForm.message}
+          ></textarea>
           <button>Send Message</button>
         </div>
       </div>
