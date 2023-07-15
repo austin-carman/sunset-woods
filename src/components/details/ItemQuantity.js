72ecd38c441a03f24d07eb17985513473bde7cc2
austin-carman <abcarman12@gmail.com>
2023-07-14 16:09:58 -1000

    added comment to ItemQuantity component

--- src/components/details/ItemQuantity.js
+++ src/components/details/ItemQuantity.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 const ItemQuantity = ({ quantity, setQuantity }) => {
+  // Increase or decrease quantity (but don't go below 1)
   const handleClickQuantity = (value) => {
     if (quantity === 1 && value === -1) {
       return;
@@ -7,6 +8,7 @@ const ItemQuantity = ({ quantity, setQuantity }) => {
     setQuantity(quantity + value);
   };
 
+  // Change quantity to value typed by user
   const handleChangeQuantity = (e) => {
     const { value } = e.target;
     setQuantity(value);
